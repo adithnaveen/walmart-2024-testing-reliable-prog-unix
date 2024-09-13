5ac70da422c2894953a2bf31a8ad4739d86731e3
Naveen <adith.naveen@gmail.com>
2024-09-13 11:50:00 +0530

    working with javascript 2

--- java-script-works/sample.js
+++ java-script-works/sample.js
@@ -2,3 +2,23 @@ function fnFirst() {
     console.log("hi you invoded fnFirst");
 
 }
+
+
+function fnSecond(myname) {
+    console.log("Hello " + myname);
+}
+
+function fnThird() {
+    myarr = ["hi", "hell", "how", "are", "you"];
+
+    for (temp in myarr) {
+        console.log(myarr[temp])
+    }
+
+}
+
+
+function fnGetData() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then(data => console.log(data));
+}
